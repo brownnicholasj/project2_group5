8fca2e4a689428d1906907b983924ef64405e04f
brownnicholasj <brown.nicholasj@gmail.com>
2021-06-19 10:37:32 -0500

    add comments to file

--- util/authorize.js
+++ util/authorize.js
@@ -1,5 +1,7 @@
 const loginCheck = (req, res, next) => {
+  //placeholder session/login
   if (!req.session.loggedIn) {
+    // placeholder path (login) until we get a correct path/view
     res.redirect('/login');
   } else {
     next();
