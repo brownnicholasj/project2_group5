1f9c9e5f9c661811d3bcb285ee66b23ebd45b727
JAPinell <japinell@yahoo.com>
2021-06-24 22:13:15 -0500

    Added GuestItem Model

---
+++ models/GuestItem.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class GuestItem extends Model {}
+
+GuestItem.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    event_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: false,
+      references: {
+        model: 'event',
+        foreignKey: 'id',
+      },
+    },
+    guest_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: false,
+      references: {
+        model: 'guest',
+        foreignKey: 'id',
+      },
+    },
+    item_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: false,
+      references: {
+        model: 'item',
+        foreignKey: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'guestitem',
+  }
+);
+
+module.exports = GuestItem;
