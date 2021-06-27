c56f490f56af0c2dbeb09734b78f689e71606630
JAPinell <japinell@yahoo.com>
2021-06-27 18:14:12 -0500

    Fixed JS error related to the logout button being now displayed conditionally

--- public/js/logout.js
+++ public/js/logout.js
@@ -14,4 +14,6 @@ const logoutHandler = async (event) => {
   }
 };
 
-document.querySelector('#logout').addEventListener('click', logoutHandler);
+if (document.querySelector('#logout')) {
+  document.querySelector('#logout').addEventListener('click', logoutHandler);
+}
